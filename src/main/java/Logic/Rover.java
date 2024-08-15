26b9f69ded7e3a22bd3a78e54aca43f120ccfeb4
jmc <120180071+sector-11@users.noreply.github.com>
2024-08-15 15:40:00 +0100

    feat: add rover rotation originally had explicit check for null and M cases, seems to not require for expected function so removed

--- src/main/java/Logic/Rover.java
+++ src/main/java/Logic/Rover.java
@@ -1,5 +1,7 @@
 package Logic;
 
+import Input.Directions;
+import Input.Instruction;
 import Input.Position;
 
 public class Rover {
@@ -7,5 +9,26 @@ public class Rover {
 
     public Rover(Position position){
         this.position = position;
+    }
+
+
+    public Directions rotate(Instruction instruction){
+        if (instruction == Instruction.L) {
+            switch (this.position.getFacing()){
+                case Directions.N -> this.position.setFacing(Directions.W);
+                case Directions.W -> this.position.setFacing(Directions.S);
+                case Directions.S -> this.position.setFacing(Directions.E);
+                case Directions.E -> this.position.setFacing(Directions.N);
+            }
+        } else if (instruction == Instruction.R) {
+            switch (this.position.getFacing()){
+                case Directions.N -> this.position.setFacing(Directions.E);
+                case Directions.E -> this.position.setFacing(Directions.S);
+                case Directions.S -> this.position.setFacing(Directions.W);
+                case Directions.W -> this.position.setFacing(Directions.N);
+            }
+        }
+
+        return this.position.getFacing();
     }
 }
