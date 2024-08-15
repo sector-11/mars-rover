03394c6279309ac33690cac658d0c50074cc6808
jmc <120180071+sector-11@users.noreply.github.com>
2024-08-15 11:56:07 +0100

    feat: add constructor for position

--- src/main/java/Input/Position.java
+++ src/main/java/Input/Position.java
@@ -4,4 +4,10 @@ public class Position {
     private int x;
     private int y;
     private Directions facing;
+
+    public Position(int x, int y, Directions facing) {
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
+    }
 }
