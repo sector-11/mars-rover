bfc3529314cfcdd39b5da8627fb9b4656890f8c1
jmc <120180071+sector-11@users.noreply.github.com>
2024-08-15 16:50:03 +0100

    feat: add getter for plateausize x and y co-ordinates also made x and y fields private

--- src/main/java/Input/PlateauSize.java
+++ src/main/java/Input/PlateauSize.java
@@ -1,12 +1,21 @@
 package Input;
 
 public class PlateauSize {
-    private int x;
-    private int y;
+    private final int x;
+
+    private final int y;
 
     public PlateauSize(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     @Override
